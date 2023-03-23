efb7d09155ac783e8f7a767ab47ef8b34eb63ced
has315 <arsen_sandic@hotmail.com>
2023-03-23 20:41:10 +0100

    add styled components, add rwrite  for client fetching

--- next.config.js
+++ next.config.js
@@ -13,6 +13,16 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*',
+        basePath: false,
+
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
