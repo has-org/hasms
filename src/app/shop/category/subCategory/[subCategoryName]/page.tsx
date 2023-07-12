cfc39dbc370c1638cce6572147332a7d81406682
has315 <arsen_sandic@hotmail.com>
2023-07-12 20:36:20 +0200

    fix build

--- src/app/shop/category/subCategory/[subCategoryName]/page.tsx
+++ src/app/shop/category/subCategory/[subCategoryName]/page.tsx
@@ -1,7 +1,7 @@
 import { ProductCard } from "@/components/ProductCard/ProductCard";
 import { StaticImageData } from "next/image";
 import { Category as CategoryType } from "@/types/Category";
-import Container from "@/components/UI/Container";
+import Container from "@/components/Container";
 import Image from "next/image";
 import NavigationCategorySection from "@/components/MUI/NavigationCategorySection";
 import { Typography } from "@mui/material";
