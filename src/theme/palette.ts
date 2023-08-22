d6c78eb32bd717f0755249ba4177e7828d418ef2
has315 <arsen_sandic@hotmail.com>
2023-08-22 20:22:30 +0200

    add blue colors to pallete

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -43,12 +43,12 @@ const PRIMARY = {
 };
 
 const SECONDARY = {
-  lighter: '#D6E4FF',
-  light: '#84A9FF',
-  main: '#3366FF',
-  dark: '#1939B7',
-  darker: '#091A7A',
-  contrastText: '#FFFFFF',
+  lighter: "#D6E4FF",
+  light: "#84A9FF",
+  main: "#0017E6",
+  dark: "#1939B7",
+  darker: "#091A7A",
+  contrastText: "#FFFFFF",
 };
 
 const INFO = {
