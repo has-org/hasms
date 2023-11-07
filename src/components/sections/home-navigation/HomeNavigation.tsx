c360c095f65b23026f5d54cbc6f9f725ece59048
has315 <arsen_sandic@hotmail.com>
2023-11-07 13:02:41 +0100

    code purge

--- src/components/sections/home-navigation/HomeNavigation.tsx
+++ src/components/sections/home-navigation/HomeNavigation.tsx
@@ -1,31 +1,38 @@
-'use client'
-import Iconify from "@/components/iconify"
-import useResponsive from "@/hooks/useResponsive"
-import { Box, Grid, Stack } from "@mui/material"
-import Typography from "@mui/material/Typography"
-import Image from "next/image"
-import Link from "next/link"
-
-const navigationItems = [
-    { icon: 'fluent-emoji-high-contrast:motorcycle', title: 'Motori', link: '/shop/category/MOTORI' },
-    { icon: 'tabler:helmet', title: 'Kacige', link: '/shop/category/KACIGE' },
-    { icon: 'mingcute:coat-line', title: 'Odjeća i obuća', link: '/shop/category/ODJEĆA-I-OBUĆA' },
-    { icon: 'bi:person-gear', title: 'Dijelovi', link: '/shop/category/DIJELOVI-I-OPREMA' },
-    { icon: 'tabler:speedboat', title: 'Gliseri', link: '/shop/category/MARINA' },
-    { icon: 'heroicons:wrench-screwdriver', title: 'Servis', link: '/shop/category/SERVIS-I-ODRŽAVANJE' },
-
-]
-
+"use client";
+import Iconify from "@/components/iconify";
+import useResponsive from "@/hooks/useResponsive";
+import { Box, Grid, Stack } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Image from "next/image";
+import Link from "next/link";
 
 const HomeNavigation = () => {
+  const isDesktop = useResponsive("up", "lg");
 
-    const isDesktop = useResponsive('up', 'lg');
+  return (
+    <Box>
+      <Stack direction={"row"}>
+        <Box>
+          <Typography></Typography>
+          <Typography></Typography>
+        </Box>
+        <Box>
+          <Image
+            className="logo-img"
+            alt="Cooperator Logo"
+            src={
+             
+                 `${process.env.NEXT_PUBLIC_API_IMG_HOST}/`
+            
+            }
+            width={500}
+            height={300}
+            style={{ objectFit: "contain" }}
+          />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
 
-    return (
-      <div className="home-front">
-
-      </div>
-    );
-}
-
-export default HomeNavigation
+export default HomeNavigation;
