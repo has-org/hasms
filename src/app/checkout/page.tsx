98f914e06179bc9de5ab7025a99bf321656322ac
has315 <arsen_sandic@hotmail.com>
2024-01-04 12:38:22 +0100

    fix build errors

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,11 +1,9 @@
-import { ProductCard } from "@/components/ProductCard/ProductCard";
 import { Product as ProductType } from "@/types/Product";
 
 
 export default async function CheckoutPage() {
 
   return (
-    <div className="mx-10 lg:ml-80 lg:mr-9 min-h-screen	 flex flex-col font-serif">
-    </div>
-  );
+    <></>
+    );
 }
