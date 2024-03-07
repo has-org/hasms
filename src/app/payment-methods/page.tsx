8068518727fb2ac4f18d9011e9b5c74aadb77cc7
dzenanramic <139631168+dzenanramic@users.noreply.github.com>
2024-03-07 16:08:59 +0100

    adding payment methods (#7)

---
+++ src/app/payment-methods/page.tsx
@@ -0,0 +1,28 @@
+import { Container, Stack, Typography } from "@mui/material";
+
+export default async function PaymentMethodsPage() {
+  return (
+    <Container maxWidth="xl">
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          py: 2,
+        }}
+      >
+        <Typography variant="h2">Načini plaćanja</Typography>
+      </Stack>
+      <Stack spacing={2}>
+        <Typography variant="body2">
+          • gotovinom (KM) prilikom dostave,
+        </Typography>
+        <Typography variant="body2">
+          • uplatom na žiro račun
+        </Typography>
+        <Typography variant="body2">
+          Kupoprodajni ugovor (narudžba) je u elektronskom obliku pohranjen na serveru ponuđača te je kupcu dostupan u svakom trenutku na njegovom korisničkom profilu (Moj Profil).
+        </Typography>
+      </Stack>
+    </Container>
+  );
+}
