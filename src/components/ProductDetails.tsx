0d1f780edff84c2d734e3ea03f321f1ae1f14cef
dzenanramic <dzenan.ramic1210@gmail.com>
2024-03-26 11:36:56 +0100

    updating single product page

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -2,10 +2,12 @@
 
 import { ColoredDot } from "@/components/ColoredDot";
 import { Color as ColorType } from "@/types/Color";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography, Container, Accordion, AccordionDetails, AccordionSummary, Divider } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { styled } from "@mui/material/styles";
 import { useContext, useState } from "react";
 import ReactSelect from "react-select";
+import Image from 'next/image';
 import { CartContext } from "@/context/CartContext/CartContext";
 import {
   Controller,
@@ -14,10 +16,12 @@ import {
   useFormContext,
 } from "react-hook-form";
 import { IProduct } from "@/types/IProduct";
+import { getProductVariants } from "@/services/apiService";
 
 type ProductProps = {
   product: IProduct;
 };
+
 
 const Text = styled(Typography)(({ theme }) => ({
   color:
@@ -45,6 +49,20 @@ export const ProductDetails = ({ product }: ProductProps) => {
   const { items, addToCart } = useContext(CartContext);
   const defaultSizePlaceholder = { value: "XS", label: "XS" };
   const { handleSubmit, control } = useForm<any>();
+  const productImages = [];
+  for (const variant of product.variants) {
+    variant.images.map(image => productImages.push(image))
+  }
+  const [count, setCount] = useState(1)
+
+  function hadleIncrease() {
+    setCount(count + 1)
+  }
+  function handleDecrease() {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  }
 
   const onSubmitHandler: SubmitHandler<any> = (
     values: any,
@@ -59,84 +77,233 @@ export const ProductDetails = ({ product }: ProductProps) => {
     addToCart({ ...product, color, size });
   };
 
+
   if (!product) return <>No product</>;
   return (
-    <Box>
-      <Text variant="h2" className="product-name">
-        {product.name}
-      </Text>
-      <Box className="product-code flex gap-x-2">
-        <Text className="product-code-key">Šifra:</Text>
-
-        <Text className="product-code-value">{product.code}</Text>
-      </Box>
-      <Box className="product-brand flex gap-x-2">
-        <Text className="product-brand-key">Brend:</Text>
-
-        <Text className="product-brand-value">{product.manufacturer}</Text>
-      </Box>
-      <Box className="product-price flex gap-x-2">
-        <Text className="product-price-key">Cijena</Text>
-
-        {/* <Text className="product-price-value">{product.price}</Text> */}
-        {/* <Text className="product-currency">{product.currency}</Text> */}
-      </Box>
-
-      <Box className="variants-wrap flex items-center gap-x-2">
-        <Text className="product-colors-title">Boja:</Text>
-        {productColors.map((color: ColorType, index: number) => {
-          return (
-            <span key={index} className="variant-color">
-              <ColoredDot color={color} />
-            </span>
-          );
-        })}
-      </Box>
-      <Box className="product-price flex gap-x-2 items-center">
-        <Text className="product-size-title">Veličina:</Text>
-        <Box component="form" onSubmit={handleSubmit(onSubmitHandler)}>
-          <Controller
-            name="size"
-            control={control}
-            rules={{ required: "Size is required" }}
-            render={({ field }) => (
-              <ReactSelect
-                isClearable
-                {...field}
-                placeholder="Select size"
-                options={productSizes.map((item) => {
-                  return { id: item.id, label: item.name, value: item.name };
-                })}
-              />
-            )}
+    <Container maxWidth="lg" >
+      <Stack direction="row" spacing={4}>
+        <Stack>
+          <Image
+            src={productImages[0].url}
+            alt="product image"
+            width={470}
+            height={352}
+            style={{ borderRadius: '16px' }}
           />
+        </Stack>
 
-          <Controller
-            name="color"
-            control={control}
-            rules={{ required: "Color is required" }}
-            render={({ field }) => (
-              <ReactSelect
-                isClearable
-                {...field}
-                placeholder="Select color"
-                options={productColors.map((item) => {
-                  return { id: item.id, label: item.name, value: item.name };
-                })}
-              />
-            )}
-          />
+        <Stack spacing={2}>
+          <Box>
+            <Text fontSize="16px" color="#ACACAC">{product.manufacturer}</Text>
+            <Text fontSize="20px" fontWeight="bold" className="product-name">
+              {product.name}
+            </Text>
+          </Box>
+          <Box>
+            <Text className="product-price-value" fontSize="28px" fontWeight="bold" color="#00D0FD">100 KM</Text>
+          </Box>
+          <Box className="variants-wrap flex items-center gap-x-2">
+            <Text className="product-colors-title" color="#ACACAC" fontSize="14px">Izaberi boju: {""}
+              {productColors.map((color: ColorType, index: number) => {
+                return (
+                  <span key={index} className="variant-color" style={{ color: "white", fontWeight: "500" }}>
+                    {color.name}
+                  </span>
+                );
+              })}
+            </Text>
+          </Box>
 
-          <Button className="" type="submit">
-            <Text className="product-description">Dodaj u korpu</Text>
-          </Button>
-        </Box>
-      </Box>
-      <Box className="product-price flex gap-x-2">
-        <Text className="product-description">{product.description}</Text>
-      </Box>
+          <Stack direction="row">
+            {product.variants[0].images.map((miniPic) => {
+              return (
+                <Image
+                  src={miniPic.url}
+                  alt="product image"
+                  width={54}
+                  height={50}
+                  style={{ borderRadius: '16px' }}
+                  key={`${product.id}_${product.variants[0].id}_${miniPic.id}`}
+                />
+              );
+            })}
+          </Stack>
 
-      <Text className="product-description-title">Opis:</Text>
-    </Box>
+
+          <Stack >
+            <Box className="product-price flex gap-x-2 items-center">
+              <Text className="product-size-title" color="#ACACAC" fontSize="14px">Veličina:</Text>
+              <Box component="form" onSubmit={handleSubmit(onSubmitHandler)}>
+                <Stack direction="row" spacing={1}>
+                  <Button
+                    variant="outlinedTransparent"
+                    color="secondary"
+                    style={{ width: '32px', height: '32px', minWidth: '32px' }}>
+                    S
+                  </Button>
+                  <Button
+                    variant="outlinedTransparent"
+                    color="secondary"
+                    style={{ width: '32px', height: '32px', minWidth: '32px' }}>
+                    M
+                  </Button>
+                  <Button
+                    variant="outlinedTransparent"
+                    color="secondary"
+                    style={{ width: '32px', height: '32px', minWidth: '32px' }}>
+                    L
+                  </Button>
+                  <Button
+                    variant="outlinedTransparent"
+                    color="secondary"
+                    style={{ width: '32px', height: '32px', minWidth: '32px' }}>
+                    XL
+                  </Button>
+                </Stack>
+              </Box>
+            </Box>
+          </Stack>
+
+          <Stack direction="row" spacing={2} >
+            <Button
+              variant="text"
+              color="secondary"
+              onClick={handleDecrease}
+              style={{ width: '32px', height: '43px', minWidth: '32px' }}>
+              -
+            </Button>
+            <Button variant="contained" disabled style={{ width: '43px', height: '43px', minWidth: '43px', color: 'white' }}>{count}</Button>
+            <Button
+              variant="text"
+              color="secondary"
+              onClick={hadleIncrease}
+              style={{ width: '32px', height: '43px', minWidth: '32px' }}>
+              +
+            </Button>
+
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="large"
+
+            >
+              <Typography fontSize="14px" fontWeight="bold">
+                + Dodaj u korpu
+              </Typography>
+            </Button>
+          </Stack>
+          <Stack
+            sx={{
+              backgroundColor: "#262626",
+              borderRadius: "16px",
+              justifyContent: "center",
+              width: { xs: "100%", md: "660px" }
+            }}
+          >
+            <Accordion>
+              <AccordionSummary
+                aria-controls="panel1-content"
+                id="panel1-header"
+                expandIcon={<ExpandMoreIcon />}
+                sx={{ height: "61px" }}
+              >
+                <Typography fontWeight={600} fontSize="18px">
+                  Specifikacije
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Stack
+                  direction="row"
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Proizvođač</Typography>
+                  <Typography variant="body2">{product.manufacturer}</Typography>
+                </Stack>
+                <Divider />
+                <Stack
+                  direction="row"
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Model</Typography>
+                  <Typography variant="body2">{product.model}</Typography>
+                </Stack>
+                <Divider />
+
+                <Stack
+                  direction="row"
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Serijski br.</Typography>
+                  <Typography variant="body2">Serijski br.Serijski br.</Typography>
+                </Stack>
+                <Divider />
+                <Stack
+                  direction="row"
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Boja</Typography>
+                  {productColors.map((color: ColorType, index: number) => {
+                    return (
+                      <Typography key={index} className="variant-color" >
+                        {color.name}
+                      </Typography>
+                    );
+                  })}
+                </Stack>
+                <Divider />
+                <Stack
+                  direction="row"
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Veličina</Typography>
+                  <Typography variant="body2">VeličinaVeličina</Typography>
+                </Stack>
+                <Divider />
+                <Stack
+                  direction="row"
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <Typography variant="body2">Dostupnost</Typography>
+                  <Typography variant="body2">DostupnostDostupnost</Typography>
+                </Stack>
+                <Divider />
+
+              </AccordionDetails>
+            </Accordion>
+          </Stack>
+
+          <Stack
+            sx={{
+              backgroundColor: "#262626",
+              borderRadius: "16px",
+              justifyContent: "center",
+            }}
+          >
+            <Accordion>
+              <AccordionSummary
+                aria-controls="panel1-content"
+                id="panel1-header"
+                expandIcon={<ExpandMoreIcon />}
+                sx={{ height: "61px" }}
+              >
+                <Typography fontWeight={600} fontSize="18px">
+                  Opis
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="h4" textAlign={"center"}>
+                  sadasd
+                </Typography>
+                <Typography variant="body1" textAlign={"justify"}>
+                  asdasdas
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </Stack>
+
+
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
