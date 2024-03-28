8173224b96c457c50349572255909f753ddd77ef
dzenanramic <139631168+dzenanramic@users.noreply.github.com>
2024-03-28 18:38:30 +0100

    update size selector component using .map function (#15)

--- src/app/shop/product/SizeSelector.tsx
+++ src/app/shop/product/SizeSelector.tsx
@@ -1,19 +1,20 @@
 'use client'
 
+import { ISize } from '@/types/ISize';
 import { Stack, Box, Typography, Button } from '@mui/material';
 import { useState } from 'react';
 
-export default function SizeSelector() {
+export default function SizeSelector({sizes}:{sizes:ISize[]}) {
 
 const[sizeSelection, setSizeSelection]=useState("M")
 
-function selectSize(e: any){
-    const selectedSize=e.target.value;
-    setSizeSelection(selectedSize);
+
+function selectSize(selectedSize: string) {
+  setSizeSelection(selectedSize);
 }
   return (
     <Stack>
-      <Box className='product-price flex gap-x-2 items-center'>
+      <Box>
         <Typography
           className='product-size-title'
           color='#ACACAC'
@@ -23,66 +24,24 @@ function selectSize(e: any){
         </Typography>
         <Box component='form'>
           <Stack direction='row' spacing={1}>
-            <Button
-              variant='outlinedTransparent'
-              color='secondary'
-              style={{ width: '32px', height: '32px', minWidth: '32px' }}
-              onClick={selectSize}
-              value='S'
-              sx={{
-                border:
-                  sizeSelection === 'S'
-                    ? '2px solid #00D0FD'
-                    : '2px solid transparent',
-              }}
-            >
-              S
-            </Button>
-            <Button
-              variant='outlinedTransparent'
-              color='secondary'
-              style={{ width: '32px', height: '32px', minWidth: '32px' }}
-              onClick={selectSize}
-              value='M'
-              sx={{
-                border:
-                  sizeSelection === 'M'
-                    ? '2px solid #00D0FD'
-                    : '2px solid transparent',
-              }}
-            >
-              M
-            </Button>
-            <Button
-              variant='outlinedTransparent'
-              color='secondary'
-              style={{ width: '32px', height: '32px', minWidth: '32px' }}
-              onClick={selectSize}
-              value='L'
-              sx={{
-                border:
-                  sizeSelection === 'L'
-                    ? '2px solid #00D0FD'
-                    : '2px solid transparent',
-              }}
-            >
-              L
-            </Button>
-            <Button
-              variant='outlinedTransparent'
-              color='secondary'
-              style={{ width: '32px', height: '32px', minWidth: '32px' }}
-              onClick={selectSize}
-              value='XL'
-              sx={{
-                border:
-                  sizeSelection === 'XL'
-                    ? '2px solid #00D0FD'
-                    : '2px solid transparent',
-              }}
-            >
-              XL
-            </Button>
+            {sizes.map((size) => (
+              <Button
+                key={size.id}
+                variant='outlinedTransparent'
+                color='secondary'
+                style={{ width: '32px', height: '32px', minWidth: '32px' }}
+                onClick={() => selectSize(size.name)}
+                value={size.name}
+                sx={{
+                  border:
+                    sizeSelection === size.name
+                      ? '2px solid #00D0FD'
+                      : '2px solid transparent',
+                }}
+              >
+                {size.name}
+              </Button>
+            ))}
           </Stack>
         </Box>
       </Box>
